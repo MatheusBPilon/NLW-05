5713d34cf7d3ca3e3c459dc536a6a733dd24cf5f
Matheus Pilon <mbpilon@gmail.com>
2021-05-28 16:47:01 -0300

    Finalização do scss

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -5,7 +5,7 @@ export function Player() {
     return (
         <div className={styles.playerContainer}>
             <header>
-                <img src="headphones.svg" alt="" width="30" />
+                <img src="/headphones.svg" alt="" width="30" />
 
                 <strong>Tocando agora</strong>
             </header>
@@ -25,19 +25,19 @@ export function Player() {
 
                 <div className={styles.buttons}>
                     <button type="button">
-                        <img src="random.svg" alt="Embaralhar" width="35" />
+                        <img src="/random.svg" alt="Embaralhar" width="35" />
                     </button>
                     <button type="button">
-                        <img src="backward.svg" alt="Tocar anterior" width="35" />
+                        <img src="/backward.svg" alt="Tocar anterior" width="35" />
                     </button>
                     <button type="button" className={styles.playButton}>
-                        <img src="play.svg" alt="Tocar" width="35" />
+                        <img src="/play.svg" alt="Tocar" width="35" />
                     </button>
                     <button type="button">
-                        <img src="forward.svg" alt="Tocar proximo" width="35" />
+                        <img src="/forward.svg" alt="Tocar proximo" width="35" />
                     </button>
                     <button type="button">
-                        <img src="repeat.svg" alt="Repetir" width="35" />
+                        <img src="/repeat.svg" alt="Repetir" width="35" />
                     </button>
                 </div>
             </footer>
